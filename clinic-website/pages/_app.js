7271fb57be3e0448c52e44032714c1beb932c4d9
Setayesh_ab <s77725@gmail.com>
2022-03-16 11:48:48 +0330

    init:global page

--- clinic-website/pages/_app.js
+++ clinic-website/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Clinic</title>
+        <link rel="icon" href="/favicon.ico" />
+        {/* <meta   http-equiv="content-type"   content="text/html;charset=utf-8"  /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- clinic-website/pages/index.js
+++ clinic-website/pages/index.js
@@ -5,11 +5,6 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Clinic</title>
-        <link rel="icon" href="/favicon.ico" />
-        {/* <meta   http-equiv="content-type"   content="text/html;charset=utf-8"  /> */}
-      </Head>
 
       <main className={styles.main}>
           Setayesh Abouei
